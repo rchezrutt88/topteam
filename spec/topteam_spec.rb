d0be7d0b5f963be2c7207896baa244a1a7976197
Robert Ruttenberg <robertruttenberg@Roberts-MacBook-Pro.local>
2021-11-09 14:12:57 -0700

    initial commit

---
+++ spec/topteam_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Topteam do
+  it "has a version number" do
+    expect(Topteam::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
