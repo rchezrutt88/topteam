a7241907bcf7ab0523c2d9590420a16543e23d92
Robert Ruttenberg <robertruttenberg@Roberts-MacBook-Pro.local>
2021-11-10 18:07:43 -0700

    WIP

--- lib/topteam.rb
+++ lib/topteam.rb
@@ -4,5 +4,85 @@ require_relative "topteam/version"
 
 module Topteam
   class Error < StandardError; end
+
+  class Team
+    attr_reader :name
+    attr_accessor :wins, :ties, :losses
+
+    def initialize(name, wins: 0, losses: 0, ties: 0)
+      @name = name
+      @wins = wins
+      @losses = losses
+      @ties = ties
+    end
+
+    def win!
+      @wins += 1
+    end
+
+    def loss!
+      @losses += 1
+    end
+
+    def tie!
+      @ties += 1
+    end
+
+    def score
+      (3 * @wins) + @ties
+    end
+  end
+
+  class Season
+    def initialize
+      @games = []
+      @teams = []
+    end
+
+    def teams; end
+
+    def match_days; end
+
+    # TODO: consider overriding <<
+    def add_game(game)
+      game.match_day = @games.count { |g| game.teams_scores[0][0] == g } + 1
+      @teams << game.team_1 unless @teams.any? { |team| team.name == game.team_1.name }
+      @teams << game.team_2 unless @teams.any? { |team| team.name == game.team_2.name }
+      @games << game
+    end
+  end
+
+  class Game
+    attr_accessor :match_day
+    attr_reader :teams_scores
+
+    def initialize(team1, team2)
+      raise TypeError unless [team1, team2].all? { |t| t[0].is_a?(String) && t[1].is_a?(Integer) }
+
+      @teams_scores = [team1, team2]
+    end
+
+    def winner
+      return if tie?
+
+      teams_scores.max_by { |ts| ts[1] }[0]
+    end
+
+    def tie?
+      teams_scores[0][1] == teams_scores[1][1]
+    end
+
+    def self.parse_game(game_string)
+      team_scores = game_string.split(", ").map { |ts| parse_team_score(ts) }
+      new(*team_scores)
+    end
+
+    def self.parse_team_score(team_score_string)
+      team_score_string.split(/\s(?=\d+$)/).tap do |array|
+        array[1] = array[1].to_i
+      end
+    end
+  end
+
   # Your code goes here...
 end
--- spec/topteam_spec.rb
+++ spec/topteam_spec.rb
@@ -4,8 +4,95 @@ RSpec.describe Topteam do
   it "has a version number" do
     expect(Topteam::VERSION).not_to be nil
   end
+  describe Topteam::Team do
+    let(:team) { Topteam::Team.new("team1") }
+    describe "#win!" do
+      it "increments the score by 3" do
+        expect { team.win! }.to change { team.score }.from(0).to(3)
+      end
+    end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+    describe "#loss!" do
+      it "does not increment the score with a loss" do
+        team.win!
+        expect(team.score).to eq(3)
+        expect { team.loss! }.not_to change { team.score }
+      end
+    end
+
+    describe "#tie!" do
+      it "increments the score by 1" do
+        expect { team.tie! }.to change { team.score }.from(0).to(1)
+      end
+    end
+
+    describe "sort_by(&:score)" do
+      let(:team1) { Topteam::Team.new("team1", wins: 2, ties: 0) }
+      let(:team2) { Topteam::Team.new("team2", wins: 1, losses: 1) }
+      let(:team3) { Topteam::Team.new("team3", ties: 1) }
+      let(:team4) { Topteam::Team.new("team4") }
+      let(:teams) { [team1, team2, team3, team4] }
+
+      it "sorts by score" do
+        expect(teams.shuffle.max_by(&:score)).to eq(team1)
+        expect(teams.shuffle.min_by(&:score)).to eq(team4)
+        expect(teams.shuffle.sort_by(&:score)).to eq([team4, team3, team2, team1])
+      end
+    end
+  end
+
+  describe Topteam::Game do
+    let(:game_string) { "San Jose Earthquakes 3, Santa Cruz Slugs 3" }
+    let(:game) { Topteam::Game.parse_game(game_string) }
+    describe ".parse_game" do
+      it "returns a game" do
+        expect(Topteam::Game.parse_game(game_string)).to be_a(Topteam::Game)
+      end
+      it "contains the tuples consisting of the team name and their game score" do
+        expect(Topteam::Game.parse_game(game_string).teams_scores).to eq([["San Jose Earthquakes", 3],
+                                                                          ["Santa Cruz Slugs", 3]])
+      end
+    end
+    describe "#tie?" do
+      it "returns true if it's a tie" do
+        expect(game.tie?).to eq(true)
+      end
+    end
+    describe "#winner" do
+      context do
+        let(:game_string) { "San Jose Earthquakes 3, Santa Cruz Slugs 4" }
+        it "returns the winning team's name" do
+          expect(game.winner).to eq("Santa Cruz Slugs")
+        end
+      end
+      it "returns nil if it's a tie" do
+        expect(game.winner).to be(nil)
+      end
+    end
+  end
+
+  describe Topteam::Season do
+    subject { Topteam::Season.new }
+    let(:input) do
+      ["San Jose Earthquakes 3, Santa Cruz Slugs 3",
+       "Capitola Seahorses 1, Aptos FC 0",
+       "Felton Lumberjacks 2, Monterey United 0",
+       "Felton Lumberjacks 1, Aptos FC 2",
+       "Santa Cruz Slugs 0, Capitola Seahorses 0",
+       "Monterey United 4, San Jose Earthquakes 2",
+       "Santa Cruz Slugs 2, Aptos FC 3",
+       "San Jose Earthquakes 1, Felton Lumberjacks 4",
+       "Monterey United 1, Capitola Seahorses 0",
+       "Aptos FC 2, Monterey United 0",
+       "Capitola Seahorses 5, San Jose Earthquakes 5",
+       "Santa Cruz Slugs 1, Felton Lumberjacks 1"]
+    end
+
+    describe "#add_game" do
+      it "parses a game string into a game object" do
+        subject.add_game(input[0])
+        expect(subject.games).to be_an_instance_of(Topteam::Game)
+      end
+    end
   end
 end
