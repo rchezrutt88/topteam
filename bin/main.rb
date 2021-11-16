1fabd38a3bbf5748869e78fe5b7cfdda76ee6518
rdrutt <robert@ruttenberg.org>
2021-11-16 15:22:54 -0500

    add entry point

---
+++ bin/main.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require_relative "../lib/topteam"
+
+lines = $stdin.readlines(chomp: true).reject { |line| line.strip.empty? }
+
+season = Topteam::Season.new
+
+season.add_games(lines) do |rankings, match_day|
+  puts "Matchday #{match_day}"
+  rankings.each do |ranking|
+    puts "#{ranking.name}, #{ranking.score} pts"
+  end
+  puts "\n"
+end
