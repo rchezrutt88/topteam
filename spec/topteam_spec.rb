8ded48c28b50dc3efad75431e957d12073211a6c
rdrutt <robert@ruttenberg.org>
2021-11-16 15:22:27 -0500

    finish tests

--- spec/topteam_spec.rb
+++ spec/topteam_spec.rb
@@ -10,33 +10,26 @@ RSpec.describe Topteam do
       it "increments the score by 3" do
         expect { team.win! }.to change { team.score }.from(0).to(3)
       end
+      it "increments matches played" do
+        expect { team.win! }.to change { team.matches_played }.from(0).to(1)
+      end
     end
-
     describe "#loss!" do
       it "does not increment the score with a loss" do
         team.win!
         expect(team.score).to eq(3)
         expect { team.loss! }.not_to change { team.score }
       end
+      it "increments matches played" do
+        expect { team.loss! }.to change { team.matches_played }.from(0).to(1)
+      end
     end
-
     describe "#tie!" do
       it "increments the score by 1" do
         expect { team.tie! }.to change { team.score }.from(0).to(1)
       end
-    end
-
-    describe "sort_by(&:score)" do
-      let(:team1) { Topteam::Team.new("team1", wins: 2, ties: 0) }
-      let(:team2) { Topteam::Team.new("team2", wins: 1, losses: 1) }
-      let(:team3) { Topteam::Team.new("team3", ties: 1) }
-      let(:team4) { Topteam::Team.new("team4") }
-      let(:teams) { [team1, team2, team3, team4] }
-
-      it "sorts by score" do
-        expect(teams.shuffle.max_by(&:score)).to eq(team1)
-        expect(teams.shuffle.min_by(&:score)).to eq(team4)
-        expect(teams.shuffle.sort_by(&:score)).to eq([team4, team3, team2, team1])
+      it "increments matches played" do
+        expect { team.tie! }.to change { team.matches_played }.from(0).to(1)
       end
     end
   end
@@ -49,8 +42,8 @@ RSpec.describe Topteam do
         expect(Topteam::Game.parse_game(game_string)).to be_a(Topteam::Game)
       end
       it "contains the tuples consisting of the team name and their game score" do
-        expect(Topteam::Game.parse_game(game_string).teams_scores).to eq([["San Jose Earthquakes", 3],
-                                                                          ["Santa Cruz Slugs", 3]])
+        expect(Topteam::Game.parse_game(game_string).team_score_tuples).to contain_exactly(["San Jose Earthquakes", 3],
+                                                                                           ["Santa Cruz Slugs", 3])
       end
     end
     describe "#tie?" do
@@ -69,13 +62,18 @@ RSpec.describe Topteam do
         expect(game.winner).to be(nil)
       end
     end
+    describe "with malformed input" do
+      it "raises a type error" do
+        expect { Topteam::Game.new([1, "Santa Cruz Slugs"], [3, "San Jose Earthquakes"]) }
+          .to raise_error(ArgumentError, /must be of form/)
+      end
+    end
   end
 
   describe Topteam::Season do
-    subject(:season) do
+    let(:initialize_season) do
       lambda { |input|
-        games = input.map { |v| Topteam::Game.parse_game(v) }
-        Topteam::Season.new(games)
+        Topteam::Season.new(input)
       }
     end
 
@@ -108,87 +106,77 @@ RSpec.describe Topteam do
       ]
     end
 
-    let(:one_day) do
-      raw_teams_array[0..2]
-    end
-
-    let(:two_days) do
-      raw_teams_array[0..5]
-    end
-
-    let(:three_days) do
-      raw_teams_array[0..8]
-    end
-
-    let(:four_days) do
-      raw_teams_array[0..11]
-    end
-
     describe "#teams" do
       it "returns an array of all the teams" do
         expected_array = expected_teams.map { |team_name| have_attributes(name: team_name) }
-        expect(season.call(raw_teams_array).teams).to contain_exactly(*expected_array)
+        expect(initialize_season.call(raw_teams_array).teams).to contain_exactly(*expected_array)
       end
     end
 
     describe "#teams_hash" do
       it "returns a hash of teams with the team names as keys" do
         expected_hash = expected_teams.each_with_object({}) { |team, hash| hash[team] = have_attributes(name: team) }
-        expect(season.call(raw_teams_array).teams_hash).to match(expected_hash)
+        expect(initialize_season.call(raw_teams_array).teams_hash).to match(expected_hash)
       end
     end
 
-    describe "#process_games" do
-      let(:initialize_season) {
-        lambda { |input|
-          games = input.map { |v| Topteam::Game.parse_game(v) }
-          Topteam::Season.new(games).process_games
-        }
-      }
-      describe "one day" do
-        subject(:teams_after_one_day) {
-          initialize_season.call(one_day).teams_tuples
-        }
-        it "increments the correct teams' scores" do
-          expect(teams_after_one_day).to contain_exactly(
-                                           ["San Jose Earthquakes", 1], ["Santa Cruz Slugs", 1], ["Capitola Seahorses", 3], ["Aptos FC", 0], ["Felton Lumberjacks", 3], ["Monterey United", 0]
-                                         )
-        end
-      end
-      describe "two days" do
-        subject(:teams_after_two_days) {
-          initialize_season.call(two_days).teams_tuples
-        }
-        it "increments the correct teams' scores" do
-          expect(teams_after_two_days).to contain_exactly(
-                                           ["San Jose Earthquakes", 1], ["Santa Cruz Slugs", 2], ["Capitola Seahorses", 4], ["Aptos FC", 3], ["Felton Lumberjacks", 3], ["Monterey United", 3]
-                                         )
-        end
-      end
-      describe "final scores" do
-        subject(:teams_after_four_days) {
-          initialize_season.call(raw_teams_array).teams_tuples
-        }
-        it "increments the correct teams' scores" do
-          expect(teams_after_four_days).to contain_exactly(
-                                            ["San Jose Earthquakes", 2], ["Santa Cruz Slugs", 3], ["Capitola Seahorses", 5], ["Aptos FC", 9], ["Felton Lumberjacks", 7], ["Monterey United", 6]
-                                          )
-        end
+    describe "#matches_per_day" do
+      it "returns the correct number of matches per day" do
+        expect(initialize_season.call(raw_teams_array).matches_per_day).to eq 3
       end
     end
-  end
 
-  describe "#add_game" do
-    xit "parses a game string into a game object" do
-      subject.add_game(input[0])
-      expect(subject.games).to be_an_instance_of(Topteam::Game)
-    end
-    it "produces the proper rankings after one day" do
-      expect(subject.call(one_day)).to eq ""
-    end
-    it "produces the proper rankings after two days" do
-      expect(subject.call(two_days)).to eq ""
+    describe "#add_games" do
+      let(:season) { Topteam::Season.new }
+      describe "yielding the rankings and match day" do
+        before do
+          expect(season).to receive(:matches_per_day=).with(3).and_call_original
+        end
+        let(:day_1_rankings) do
+          [have_attributes(name: "Capitola Seahorses", score: 3),
+           have_attributes(name: "Felton Lumberjacks", score: 3),
+           have_attributes(name: "San Jose Earthquakes", score: 1)]
+        end
+        let(:day_2_rankings) do
+          [
+            have_attributes(name: "Capitola Seahorses", score: 4),
+            have_attributes(name: "Aptos FC", score: 3),
+            have_attributes(name: "Felton Lumberjacks", score: 3)
+          ]
+        end
+        let(:day_3_rankings) do
+          [
+            have_attributes(name: "Aptos FC", score: 6),
+            have_attributes(name: "Felton Lumberjacks", score: 6),
+            have_attributes(name: "Monterey United", score: 6)
+          ]
+        end
+        let(:day_4_rankings) do
+          [
+            have_attributes(name: "Aptos FC", score: 9),
+            have_attributes(name: "Felton Lumberjacks", score: 7),
+            have_attributes(name: "Monterey United", score: 6)
+          ]
+        end
+        it "yields the rankings once" do
+          expect { |b| season.add_games(raw_teams_array[0..3], &b) }.to yield_with_args(day_1_rankings, 1)
+        end
+        it "yields the rankings twice" do
+          expect do |b|
+            season.add_games(raw_teams_array[0..6], &b)
+          end.to yield_successive_args([day_1_rankings, 1], [day_2_rankings, 2])
+        end
+        it "yields the rankings three times" do
+          expect do |b|
+            season.add_games(raw_teams_array[0..9], &b)
+          end.to yield_successive_args([day_1_rankings, 1], [day_2_rankings, 2], [day_3_rankings, 3])
+        end
+        it "yields the correct rankings four times" do
+          expect do |b|
+            season.add_games(raw_teams_array, &b)
+          end.to yield_successive_args([day_1_rankings, 1], [day_2_rankings, 2], [day_3_rankings, 3], [day_4_rankings, 4])
+        end
+      end
     end
   end
 end
-
